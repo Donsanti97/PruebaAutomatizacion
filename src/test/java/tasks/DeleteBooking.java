136de99e61a95f98b80fe25f58d3919be476f8c4
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2024-05-27 16:46:06 -0500

    DeleteBooking class updated

--- src/test/java/tasks/DeleteBooking.java
+++ src/test/java/tasks/DeleteBooking.java
@@ -5,6 +5,8 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.rest.interactions.Delete;
 import net.thucydides.core.annotations.Step;
 
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
 public class DeleteBooking implements Task {
 
     private final int bookingId;
@@ -15,20 +17,21 @@ public class DeleteBooking implements Task {
         this.token = token;
     }
 
-    @Step("{0} deletes the booking with id #bookingId")
+    @Step("{0} deletes the booking for booking id #bookingId")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Delete.from("/booking/{id}")
                         .with(request -> request
-                                .pathParam("id", bookingId)
                                 .header("Cookie", "token=" + token)
+                                .pathParam("id", bookingId)
                         )
         );
     }
 
     public static DeleteBooking byId(int bookingId, String token) {
-        return new DeleteBooking(bookingId, token);
+        return instrumented(DeleteBooking.class, bookingId, token);
     }
 }
+
 
 
