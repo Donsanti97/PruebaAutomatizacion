f979695c20340b8ce13131e8cb71d876cc64e9ab
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2024-05-27 16:45:54 -0500

    GetBooking class updated

--- src/test/java/tasks/GetBooking.java
+++ src/test/java/tasks/GetBooking.java
@@ -5,6 +5,8 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.rest.interactions.Get;
 import net.thucydides.core.annotations.Step;
 
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
 public class GetBooking implements Task {
 
     private final int bookingId;
@@ -13,17 +15,17 @@ public class GetBooking implements Task {
         this.bookingId = bookingId;
     }
 
-    @Step("{0} gets the booking details for booking id #bookingId")
+    @Step("{0} fetches the booking details for booking id #bookingId")
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Get.resource("/booking/{id}")
-                        .with(request -> request.pathParam("id", bookingId))
+                Get.resource("/booking/{id}").with(request -> request.pathParam("id", bookingId))
         );
     }
 
     public static GetBooking byId(int bookingId) {
-        return new GetBooking(bookingId);
+        return instrumented(GetBooking.class, bookingId);
     }
 }
+
 
 
