6fff888fa2ff643a3c2800aaf31d0f30bf17e185
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2024-05-27 16:45:32 -0500

    UpdateBooking class updated

--- src/test/java/tasks/UpdateBooking.java
+++ src/test/java/tasks/UpdateBooking.java
@@ -6,27 +6,19 @@ import net.serenitybdd.screenplay.rest.interactions.Put;
 import net.thucydides.core.annotations.Step;
 import io.restassured.http.ContentType;
 
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
 public class UpdateBooking implements Task {
 
     private final int bookingId;
     private final String firstname;
     private final String lastname;
-    private final int totalprice;
-    private final boolean depositpaid;
-    private final String checkin;
-    private final String checkout;
-    private final String additionalneeds;
     private final String token;
 
-    public UpdateBooking(int bookingId, String firstname, String lastname, int totalprice, boolean depositpaid, String checkin, String checkout, String additionalneeds, String token) {
+    public UpdateBooking(int bookingId, String firstname, String lastname, String token) {
         this.bookingId = bookingId;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.totalprice = totalprice;
-        this.depositpaid = depositpaid;
-        this.checkin = checkin;
-        this.checkout = checkout;
-        this.additionalneeds = additionalneeds;
         this.token = token;
     }
 
@@ -38,14 +30,17 @@ public class UpdateBooking implements Task {
                                 .contentType(ContentType.JSON)
                                 .header("Cookie", "token=" + token)
                                 .pathParam("id", bookingId)
-                                .body("{\"firstname\":\"" + firstname + "\",\"lastname\":\"" + lastname + "\",\"totalprice\":" + totalprice + ",\"depositpaid\":" + depositpaid + ",\"bookingdates\":{\"checkin\":\"" + checkin + "\",\"checkout\":\"" + checkout + "\"},\"additionalneeds\":\"" + additionalneeds + "\"}")
+                                .body("{\"firstname\":\"" + firstname + "\",\"lastname\":\"" + lastname + "\"}")
                         )
         );
     }
 
-    public static UpdateBooking withDetails(int bookingId, String firstname, String lastname, int totalprice, boolean depositpaid, String checkin, String checkout, String additionalneeds, String token) {
-        return new UpdateBooking(bookingId, firstname, lastname, totalprice, depositpaid, checkin, checkout, additionalneeds, token);
+    public static UpdateBooking withDetails(int bookingId, String firstname, String lastname, String token) {
+        return instrumented(UpdateBooking.class, bookingId, firstname, lastname, token);
     }
 }
+
+
+//.body("{\"firstname\":\"" + firstname + "\",\"lastname\":\"" + lastname + "\",\"totalprice\":" + totalprice + ",\"depositpaid\":" + depositpaid + ",\"bookingdates\":{\"checkin\":\"" + checkin + "\",\"checkout\":\"" + checkout + "\"},\"additionalneeds\":\"" + additionalneeds + "\"}")
 
 
