c08009d6173ec46036605d1723c8ae58d8b35fb6
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2024-05-27 16:45:43 -0500

    LastResponse class updated

--- src/test/java/question/LastResponse.java
+++ src/test/java/question/LastResponse.java
@@ -1,28 +1,7 @@
 package question;
 
-/*import io.restassured.response.Response;
-import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.rest.questions.TheResponse;
-//import net.serenitybdd.screenplay.rest.questions.LastResponse;
-
-public class LastResponse {
-    public static Question<Integer> statusCode() {
-        return actor -> TheResponse.statusCode().answeredBy(actor);
-    }
-
-    public static Question<String> field(String fieldName) {
-        return actor -> net.serenitybdd.screenplay.rest.questions.LastResponse.received().answeredBy(actor).jsonPath().getString(fieldName);
-    }
-
-    public static Question<Response> body() {
-        return actor -> net.serenitybdd.screenplay.rest.questions.LastResponse.received().answeredBy(actor)*//*TheResponse.received().answeredBy(actor)*//*;
-    }
-}*/
 import net.serenitybdd.rest.SerenityRest;
-import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.rest.questions.ResponseConsequence;
-import net.serenitybdd.screenplay.rest.questions.RestQueryFunction;
 
 public class LastResponse {
 
@@ -38,5 +17,7 @@ public class LastResponse {
         return actor -> SerenityRest.lastResponse().getBody().asString();
     }
 }
+
+
 
 
