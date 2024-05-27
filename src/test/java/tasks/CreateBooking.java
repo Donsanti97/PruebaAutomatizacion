039396bdab9de9ff1928392534201e553a9b8064
Mairon Santiago Martinez Rojas <msmartinez880@misena.edu.co>
2024-05-27 16:46:25 -0500

    CreateBooking class updated

--- src/test/java/tasks/CreateBooking.java
+++ src/test/java/tasks/CreateBooking.java
@@ -3,28 +3,19 @@ package tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.rest.interactions.Post;
-import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.thucydides.core.annotations.Step;
 import io.restassured.http.ContentType;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class CreateBooking implements Task {
 
     private final String firstname;
     private final String lastname;
-    private final int totalprice;
-    private final boolean depositpaid;
-    private final String checkin;
-    private final String checkout;
-    private final String additionalneeds;
 
-    public CreateBooking(String firstname, String lastname, int totalprice, boolean depositpaid, String checkin, String checkout, String additionalneeds) {
+    public CreateBooking(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this.totalprice = totalprice;
-        this.depositpaid = depositpaid;
-        this.checkin = checkin;
-        this.checkout = checkout;
-        this.additionalneeds = additionalneeds;
     }
 
     @Step("{0} creates a new booking for #firstname #lastname")
@@ -33,14 +24,15 @@ public class CreateBooking implements Task {
                 Post.to("/booking")
                         .with(request -> request
                                 .contentType(ContentType.JSON)
-                                .body("{\"firstname\":\"" + firstname + "\",\"lastname\":\"" + lastname + "\",\"totalprice\":\"" + totalprice + "\",\"depositpaid\":\"" + depositpaid + "\",\"bookingdates\":{\"checkin\":\"" + checkin + "\",\"checkout\":\"" + checkout + "\"},\"additionalneeds\":\"" + additionalneeds + "\"}")
+                                .body("{\"firstname\":\"" + firstname + "\",\"lastname\":\"" + lastname + "\"}")
                         )
         );
     }
 
-    public static CreateBooking withDetails(String firstname, String lastname, int totalprice, boolean depositpaid, String checkin, String checkout, String additionalneeds) {
-        return new CreateBooking(firstname, lastname, totalprice, depositpaid, checkin, checkout, additionalneeds);
+    public static CreateBooking withDetails(String firstname, String lastname) {
+        return instrumented(CreateBooking.class, firstname, lastname);
     }
 }
+
 
 
